3973fbc8564ee2b4598c77523a499beb22982624
Yaba <211063265@aluno.unb.br>
2023-03-06 11:09:55 -0300

    feat: arrumando o pickData da data, pegando os inputs dos horários e adicionando o componente Toast com uma pequena verificação

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonDatetime } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 import { format, parseISO } from 'date-fns';
 
 @Component({
@@ -9,21 +10,33 @@ import { format, parseISO } from 'date-fns';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private toastController: ToastController) {
+    this.dataAtual();
+  }
 
   @ViewChild(IonDatetime)
   datetime!: IonDatetime;
-  dataAtualFormatada = format(new Date(), 'dd/MM/yyyy');
-  horarioAtualFormatado = format(new Date(), 'HH:mm');
-  inputNota:string = '';
+  valorData = format(new Date(), 'dd/MM/yyyy');
+  value = "";
+  inputData : string = '';
+  inputNota :string = '';
   nota : string = '';
-  
+  showPicker = false;
+  inputHorarioI = '';
+  inputHorarioF = '';
+  horarioI = '';
+  horarioF = '';
 
   dataFormatada = '';
 
-  dataMudou(valor: string) {
-    console.log(valor);
-    this.dataFormatada = format(parseISO(valor), 'HH:mm, dd/MM/yyyy');
+  dataAtual() {
+    this.dataFormatada = format(new Date(), 'dd/MM/yyyy');
+  }
+
+  dataMudou(value: any) {
+    this.valorData = value;
+    this.dataFormatada = format(parseISO(value), 'dd/MM/yyyy');
+    this.showPicker = false;
   }
 
   fechar() {
@@ -32,11 +45,87 @@ export class Tab2Page {
 
   confirmar() {
     this.datetime.confirm(true);
+    console.log(this.dataFormatada);
+    console.log(this.valorData);
   }
 
   salvarNota() {
+    if (this.inputNota == '') {
+      this.nota = 'Sem nota';
+      return true
+    }
+
     this.nota = this.inputNota;
     console.log(this.nota);
     this.inputNota = '';
+    return true
   }
+
+  salvarHorarioI() {
+    if (this.inputHorarioI == '') {
+      return false
+    }
+
+    this.horarioI = this.inputHorarioI;
+    console.log(this.horarioI);
+    this.inputHorarioI = '';
+    return true;
+  }
+
+  salvarHorarioF() {
+    if (this.inputHorarioF == '') {
+      return false
+    }
+
+    this.horarioF = this.inputHorarioF;
+    console.log(this.horarioF);
+    this.inputHorarioF = '';
+    return true;
+  }
+
+  async avisoToast() {
+    const toast = await this.toastController.create({
+      message: 'Preencha todos os campos',
+      duration: 1500,
+      color: 'danger',
+      position: 'bottom'
+    });
+
+    await toast.present();
+  }
+
+  async confirmaçãoToast() {
+    const toast = await this.toastController.create({
+      message: 'Agendamento salvo com sucesso!',
+      duration: 1500,
+      color: 'success',
+      position: 'bottom'
+    });
+
+    await toast.present();
+  }
+
+  async erroCamposToast() {
+    const toast = await this.toastController.create({
+      message: 'Verifique os campos corretamente',
+      duration: 1500,
+      color: 'danger',
+      position: 'bottom'
+    });
+
+    await toast.present();
+  }
+
+  salvarAgendamento() {
+    
+    if (this.inputNota == '' || this.inputHorarioI == '' || this.inputHorarioF == '') {
+      this.avisoToast();
+    } else {
+      let count = 0;
+      this.salvarHorarioI() ? count : count++;
+      this.salvarHorarioF() ? count : count++;
+  
+      count <=0 ? this.confirmaçãoToast() : this.erroCamposToast()
+    }
+  } 
 }
