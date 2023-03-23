4e33d5319019c463e3252f2322fc54669bb7ce1c
Vinícius Mendes Martins <211063265@aluno.unb.br>
2023-03-23 16:02:35 -0300

    feat: adicionando o pacote mqtt que acende e desliga o led através de uma mensagem pelo mqtt

---
+++ esp32_mqtt_service/src/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+
+#include "wifi.h"
+#include "mqtt.h"
+
+SemaphoreHandle_t conexaoWifiSemaphore;
+SemaphoreHandle_t conexaoMQTTSemaphore;
+
+void conectadoWifi(void * params)
+{
+  while(true)
+  {
+    if(xSemaphoreTake(conexaoWifiSemaphore, portMAX_DELAY))
+    {
+      // Processamento Internet
+      mqtt_start();
+    }
+  }
+}
+
+void trataComunicacaoComServidor(void * params)
+{
+  //char mensagem[50];
+  if(xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY))
+  {
+    while(true)
+    {
+      mqtt_envia_mensagem("ele/irragador", "1");
+      mqtt_recebe_mensagem("ele/irragador");
+
+
+      vTaskDelay(15000 / portTICK_PERIOD_MS);
+
+      mqtt_envia_mensagem("ele/irragador", "0");
+      mqtt_recebe_mensagem("ele/irragador");
+
+      
+      vTaskDelay(3000 / portTICK_PERIOD_MS);
+    }
+  }
+}
+
+void app_main(void)
+{
+    // Inicializa o NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    conexaoWifiSemaphore = xSemaphoreCreateBinary();
+    conexaoMQTTSemaphore = xSemaphoreCreateBinary();
+    wifi_start();
+
+    xTaskCreate(&conectadoWifi,  "Conexão ao MQTT", 4096, NULL, 1, NULL);
+    xTaskCreate(&trataComunicacaoComServidor, "Comunicação com Broker", 4096, NULL, 1, NULL);
+}
